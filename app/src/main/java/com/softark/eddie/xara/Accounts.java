b2e9f053fd18ebfcc175cd0eb9c5dc124cee9142
Oirere-Jr <eoirere8@gmail.com>
2017-03-08 12:39:35 +0300

    login screen

--- app/src/main/java/com/softark/eddie/xara/Accounts.java
+++ app/src/main/java/com/softark/eddie/xara/Accounts.java
@@ -2,6 +2,8 @@ package com.softark.eddie.xara;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class Accounts extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class Accounts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
